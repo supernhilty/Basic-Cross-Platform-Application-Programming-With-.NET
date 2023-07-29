9839664639a4334ab2beffa6ee079eff3b4b92e5
supernhilty <nhilty302814@gmail.com>
2023-07-29 15:48:01 +0700

    lab 3

---
+++ Hands-on Labs/AutoMobileSolution3/AutoMobileLibrary/DataAccess/MyStockContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoMobileLibrary.DataAccess;
+
+public partial class MyStockContext : DbContext
+{
+    public MyStockContext()
+    {
+    }
+
+    public MyStockContext(DbContextOptions<MyStockContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Car> Cars { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=12345;database=MyStock;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Car>(entity =>
+        {
+            entity.ToTable("Car");
+
+            entity.Property(e => e.CarId).HasColumnName("CarID");
+            entity.Property(e => e.CarName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Manufacturer)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Price).HasColumnType("money");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Hands-on Labs/AutoMobileSolution3/AutoMobileLibrary/Repository/ICarRepository.cs
@@ -0,0 +1,15 @@
+﻿
+
+using AutoMobileLibrary.DataAccess;
+
+namespace AutoMobileLibrary.Repository
+{
+    public interface ICarRepository
+    {
+        IEnumerable<Car> GetCars();
+        Car GetCarByID(int carId);
+        void InsertCar(Car car);
+        void UpdateCar(Car car);
+        void DeleteCar(int carId);
+    }
+}
